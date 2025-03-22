953e4c1982016b3047ce150226a48fa7b394d730
Chetan Rathod <chetan0101career@gmail.com>
2025-03-22 12:21:48 +0530

    env setup

--- ZealERP/settings.py
+++ ZealERP/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,10 +90,10 @@ WSGI_APPLICATION = 'ZealERP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "zeal",
+        'NAME': os.getenv("DATABASE_NAME"),
         'HOST': "localhost",
-        'USER': "root",
-        'PASSWORD': "Shruti14@",
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
         'PORT':"3306"
     }
 }
